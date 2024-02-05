9525504c9242af707834510af3bca78a6c2b0def
Letgatte <samuel.voltolini@icloud.com>
2024-02-05 11:49:32 +0000

    Update

--- YaztApp.swift
+++ YaztApp.swift
@@ -1,14 +1,24 @@
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
+
 
 @main
 struct YaztApp: App {
-    let persistenceController = PersistenceController.shared
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
 
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
